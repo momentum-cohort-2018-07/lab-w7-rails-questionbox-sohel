cebfe5a464a095be629eba16b69701864d47822d
spatel1009 <spatel1009@gmail.com>
2018-09-26 19:29:07 -0400

    small changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
   skip_before_action :verify_authentication
   
   def index
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
       render :new 
     end
   end
+
 
   def show
     @post = Post.find(params[:id])
